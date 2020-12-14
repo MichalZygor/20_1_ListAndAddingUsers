6fd335bc28a937461d35c2f4abf8458ee78fcbfd
mzygor <michal@winami.com>
2020-12-14 18:11:49 +0100

    Open Project

---
+++ src/test/java/pl/slask/ovo/listandaddingusers/git/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.slask.ovo.listandaddingusers.git.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
